e347fdc62c56addfaa1cdb87f16bcd87660ce2b6
JMax45 <maxmailru.45@gmail.com>
2020-08-17 14:21:46 +0200

    Added media and caption step

--- src/scenes/schedule.js
+++ src/scenes/schedule.js
@@ -1,11 +1,28 @@
 const WizardScene = require('telegraf/scenes/wizard');
-const exampleWizard = new WizardScene(
+const schedule = new WizardScene(
   'schedule',
   ctx => {
-    ctx.reply("This function is yet to be implemented");
+    ctx.reply("Send me the media of the post");
     ctx.wizard.state.data = {};
+    return ctx.wizard.next();
+  },
+  ctx => {
+    const { photo, video } = ctx.message;
+    ctx.wizard.state.data.media = photo ? { type: 'photo', file: photo[photo.length-1] } : { type: 'video', file: video };
+
+    if(ctx.wizard.state.data.media.file===undefined){
+      ctx.reply('Invalid media. Please retry');
+    }
+    else{
+      ctx.reply("Send me the caption of the post");
+      return ctx.wizard.next();
+    }
+  },
+  ctx => {
+    ctx.wizard.state.data.caption = ctx.message.text;
+    console.log(ctx.wizard.state.data);
     return ctx.scene.leave();
   }
 );
 
-module.exports = exampleWizard;
+module.exports = schedule;
